6a7e1566a6ec35bf8a66eea36aecd8cd5557f067
AndrewSalygin <andrew.salygin@gmail.com>
2020-08-08 17:52:54 +0400

    Did hit checkers

--- include/rules.h
+++ include/rules.h
@@ -1,8 +1,10 @@
 #ifndef RULES_H
 #define RULES_H
-#include <iostream>
 #include "data.h"
+#include "actions_on_array.h"
 
+bool hit_checker(Desk *desk, int &next_line, int &next_column, int &hit_column,
+		int &hit_line);
 bool step_checker(Desk *desk, Coordinate &matrix_c, Coordinate &matrix_n,
 		int &next_line);
 bool rules(Desk *desk, Coordinate &matrix_c, Coordinate &matrix_n, 
--- src/rules.cpp
+++ src/rules.cpp
@@ -1,5 +1,17 @@
 #include "../include/rules.h"
 
+bool hit_checker(Desk *desk, int &next_line, int &next_column, int &hit_column,
+		int &hit_line, Coordinate matrix_n)
+{
+	if (matrix_n.y == hit_line && matrix_n.x == hit_column &&
+		(*desk)[next_line][next_column].figure_type != Empty)
+	{
+		clear_element((*desk)[next_line][next_column]);
+		return true;
+	}
+	return false;
+}
+
 bool step_checker(Desk *desk, Coordinate &matrix_c, Coordinate &matrix_n,
 		int &next_line)
 {
@@ -18,29 +30,48 @@ bool rules(Desk *desk, Coordinate &matrix_c, Coordinate &matrix_n,
 	// Если выбранная фигура шашка
 	if ((*desk)[matrix_c.y][matrix_c.x].figure_type == Checker)
 	{
+		// Следующая линия, на которую ступит пешка
 		int next_line;
+		int next_column;
+		int hit_line;
+		int hit_column;
 		if ((*desk)[matrix_c.y][matrix_c.x].figure_color == White)
 		{
 			// Подготовка переменных для выбранного цвета
 			next_line = matrix_c.y + 1;
-
-			// Шаг:
-			if (step_checker(desk, matrix_c, matrix_n, next_line))
-			{
-				return true;
-			}
+			next_column = matrix_c.x + 1;
+			hit_line = matrix_c.y + 2;
+			hit_column = matrix_c.x + 2;
 		}
 		else
 		{
 			// Подготовка переменных для выбранного цвета
 			next_line = matrix_c.y - 1;
-			
-			// Шаг:
-			if (step_checker(desk, matrix_c, matrix_n, next_line))
-			{
-				return true;
-			}
+			next_column = matrix_c.x + 1;
+			hit_line = matrix_c.y - 2;
+			hit_column = matrix_c.x + 2;
 		}
+		// Шаг:
+		if (step_checker(desk, matrix_c, matrix_n, next_line))
+		{
+			return true;
+		}
+
+		// Удар:
+		if (hit_checker(desk, next_line, next_column, hit_column, hit_line,
+				matrix_n))
+		{
+			return true;
+		}
+		next_column = matrix_c.x - 1;
+		hit_column  = matrix_c.x - 2;
+		if (hit_checker(desk, next_line, next_column, hit_column, hit_line,
+				matrix_n))
+		{
+			return true;
+		}
+		// Множественный удар
+
 	}
 	// Если выбранная фигура дамка
 	if ((*desk)[matrix_c.y][matrix_c.x].figure_type == King)
